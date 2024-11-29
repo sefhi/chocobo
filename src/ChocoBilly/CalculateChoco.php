b90ae9adfa756c2601f84dbf7607433198150796
jmalias <jm.alias@covermanager.com>
2024-11-29 21:54:33 +0100

    Feat: finish tests calculateMinimumChocobos

--- src/ChocoBilly/CalculateChoco.php
+++ src/ChocoBilly/CalculateChoco.php
@@ -6,13 +6,33 @@ namespace App\ChocoBilly;
 
 final class CalculateChoco
 {
+    private const int MAX_WEIGHT = PHP_INT_MAX;
 
-    public function __construct()
+    public function calculateMinimumChocobos(int $weight, array $chocobos): array
     {
-    }
+        $minChocobos  = array_fill(0, $weight + 1, self::MAX_WEIGHT);
+        $combinations = array_fill(0, $weight + 1, []);
 
-    public function calculateMinimumChocobos(int $weight, array $chocobos)
-    {
-        return ['2', '3'];
+        $minChocobos[0] = 0;
+
+        for ($i = 1; $i <= $weight; $i++) {
+            foreach ($chocobos as $chocoboWeight) {
+
+                if ($i >= $chocoboWeight && $minChocobos[$i - $chocoboWeight] != self::MAX_WEIGHT) {
+
+                    $newCount = $minChocobos[$i - $chocoboWeight] + 1;
+
+                    if ($minChocobos[$i] > $newCount) {
+
+                        $minChocobos[$i]    = $newCount;
+                        $combinations[$i]   = $combinations[$i - $chocoboWeight];
+                        $combinations[$i][] = $chocoboWeight;
+                    }
+                }
+            }
+        }
+
+        sort($combinations[$weight]);
+        return $combinations[$weight];
     }
 }
--- tests/ChocoBilly/CalculateChocoTest.php
+++ tests/ChocoBilly/CalculateChocoTest.php
@@ -5,20 +5,47 @@ declare(strict_types=1);
 namespace App\Tests\ChocoBilly;
 
 use App\ChocoBilly\CalculateChoco;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class CalculateChocoTest extends TestCase
 {
-    #[Test]
-    public function itShouldCalculateMinimumNumberChocobosPerWeight(): void
+    public static function provideData(): \Iterator
     {
-        // GIVEN
+        yield 'When weight is 6 and chocobos are 2 and 3, then minimum number of chocobos is 3 and 3' => [
+            6,
+            [2,3],
+            [3, 3]
+        ];
 
-        $weight = 6;
-        $chocobos = [2,3];
-        $minimumChocobosExpected = [2,3];
+        yield 'When weight is 3 and chocobos are 1 and 2, then minimum number of chocobos is 1 and 2' => [
+            3,
+            [1,2],
+            [1,2]
+        ];
 
+        yield 'When weight is 5 and chocobos are 2 and 3, then minimum number of chocobos is 2 and 3' => [
+            5,
+            [2,3],
+            [2,3]
+        ];
+
+        yield 'When weight is 9 and chocobos are 1, 2 and 3, then minimum number of chocobos is 1, 2, 2, 2 and 2' => [
+            9,
+            [1,2],
+            [1,2,2,2,2]
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('provideData')]
+    public function itShouldCalculateMinimumNumberChocobosPerWeight(
+        int $weight,
+        array $chocobos,
+        array $minimumChocobosExpected
+    ): void
+    {
         // WHEN
 
         $calculateChocobo = new CalculateChoco();
