a43816845f81f108f91554dfb1659df1d97bd165
jmalias <jm.alias@covermanager.com>
2024-11-30 11:27:09 +0100

    Feat: improvements in script

--- src/ChocoBilly/ChocoScript.php
+++ src/ChocoBilly/ChocoScript.php
@@ -14,3 +14,12 @@ $calculateChoco = new ChocoCalculate();
 $chocoBilly = new ChocoBilly($calculateChoco);
 
 $chocoBilly($inputFilePath, $outputFilePath);
+
+$exist = file_exists($outputFilePath);
+if (false === $exist) {
+    echo "Error creating file ❌";
+}
+
+echo "File output created successfully ✅" . PHP_EOL . PHP_EOL;
+echo 'Input: ' . PHP_EOL . file_get_contents($inputFilePath) . PHP_EOL . PHP_EOL;
+echo 'Output: '. PHP_EOL . file_get_contents($outputFilePath) . PHP_EOL;
