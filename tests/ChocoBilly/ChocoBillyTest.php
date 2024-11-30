690a97e881078ed1100e8ecee30accb20e5859c6
jmalias <jm.alias@covermanager.com>
2024-11-30 09:20:20 +0100

    Refactor: remove output at to finish tests

--- tests/ChocoBilly/ChocoBillyTest.php
+++ tests/ChocoBilly/ChocoBillyTest.php
@@ -8,6 +8,13 @@ use PHPUnit\Framework\TestCase;
 
 class ChocoBillyTest extends TestCase
 {
+    private string $outputFilePath;
+
+    protected function setUp(): void
+    {
+        $this->outputFilePath = __DIR__ . '/outputTest.txt';
+    }
+
     #[Test]
     public function itShouldReturnAnFileOutputCorrectly(): void
     {
@@ -15,19 +22,25 @@ class ChocoBillyTest extends TestCase
         // GIVEN
 
         $inputFilePath  = __DIR__ . '/inputTest.txt';
-        $outputFilePath = __DIR__ . '/outputTest.txt';
 
         $outputFilePathExpected = __DIR__ . '/outputTestExpected.txt';
 
         // WHEN
 
         $chocoBilly = new ChocoBilly();
-        $output = ($chocoBilly)($inputFilePath, $outputFilePath);
+        $output = ($chocoBilly)($inputFilePath, $this->outputFilePath);
 
         // THEN
 
-        self::assertFileEquals($outputFilePathExpected, $outputFilePath);
+        self::assertFileEquals($outputFilePathExpected, $this->outputFilePath);
 
+    }
+
+    protected function tearDown(): void
+    {
+        if (file_exists($this->outputFilePath)) {
+            unlink($this->outputFilePath);
+        }
     }
 
 }
